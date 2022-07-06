bcb44db223946b5d84d9a65d7332c2a036268713
Alexey Radionenko <radionenko.alexey@gmail.com>
2022-07-06 11:45:08 +0300

    Упражнение: Пример маскирования данных с помощью регулярки (добавил коммент)

--- src/main/java/exercise/regexp/MaskRegExpApp.java
+++ src/main/java/exercise/regexp/MaskRegExpApp.java
@@ -5,9 +5,12 @@ import java.util.regex.Pattern;
 
 import static utils.Utils.println;
 
+/**
+ * Пример использования регулярных выражений для маскирования данных
+ */
 public class MaskRegExpApp {
 
-    private static final String MASK = "$1******$3";
+    private static final String MASK = "$1******$3"; //$1 и $3 это группы из регулярки ниже
     private static final Pattern PATTERN = Pattern.compile("([0-9]{6})([0-9]{6})([0-9]{4})");
 
     public static void main(String[] args) {
